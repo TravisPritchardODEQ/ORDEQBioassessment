6f0dd44fec0c01f4e06d9bc5db8710b29453b3c5
Travis Pritchard <travis.pritchard@deq.oregon.gov>
2026-07-01 09:26:13 -0700

    Typo in attribute table
    Make sure Insecta is not all caps.

--- data-raw/attribute_table.R
+++ data-raw/attribute_table.R
@@ -1,7 +1,13 @@
+library(tidyverse)
+
 
 attribute_table_loc = 'https://github.com/leppott/BioMonTools_SupportFiles/raw/refs/heads/main/data/taxa_official/ORWA/old/ORWA_Attributes_20241121.csv'
 
 attribute_table <- read.csv(attribute_table_loc)
+
+
+attribute_table <- attribute_table |>
+  mutate(Class = ifelse(Class == 'INSECTA', 'Insecta', Class))
 
 
 usethis::use_data(attribute_table, overwrite = TRUE)
